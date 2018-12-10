a641778d4f4637796022106c5e890bd24a1d92b6
Fernando Oliveira <nandooliveira.al@gmail.com>
2018-12-10 00:38:52 -0300

    database classes

--- examples/atoms_of_confusion/analysis.py
+++ examples/atoms_of_confusion/analysis.py
@@ -8,43 +8,34 @@ __author__ = "Edwin Dalmaijer"
 import os
 
 # custom
-from pygazeanalyser.edfreader import read_edf
 from pygazeanalyser.gazeplotter import draw_fixations, draw_heatmap, draw_scanpath, draw_raw
 
 # external
 import time
 import numpy
 
+from database import easy
 
 # # # # #
 # CONSTANTS
 
 # PARTICIPANTS
-PPS = ['gaze_data']
+PPS = [1]
 
 # DIRECTORIES
 # paths
 DIR = os.path.dirname(__file__)
 IMGDIR = os.path.join(DIR, 'imgs')
-DATADIR = os.path.join(DIR, 'data')
 PLOTDIR = os.path.join(DIR, 'plots')
 OUTPUTFILENAME = os.path.join(DIR, "output.txt")
+
 # check if the image directory exists
 if not os.path.isdir(IMGDIR):
-	raise Exception("ERROR: no image directory found; path '%s' does not exist!" % IMGDIR)
-# check if the data directory exists
-if not os.path.isdir(DATADIR):
-	raise Exception("ERROR: no data directory found; path '%s' does not exist!" % DATADIR)
+    raise Exception("ERROR: no image directory found; path '%s' does not exist!" % IMGDIR)
+
 # check if output directorie exist; if not, create it
 if not os.path.isdir(PLOTDIR):
-	os.mkdir(PLOTDIR)
-
-# DATA FILES
-SEP = ',' # value separator
-EDFSTART = "TRIALSTART" # EDF file trial start message
-EDFSTOP = "TRIALEND" # EDF file trial end message
-TRIALORDER = [EDFSTART, 'image online','image offline', EDFSTOP]
-INVALCODE = 0.0 # value coding invalid data
+    os.mkdir(PLOTDIR)
 
 # EXPERIMENT SPECS
 DISPSIZE = (1920,1080) # (px,px)
@@ -52,122 +43,58 @@ SCREENSIZE = (39.9,29.9) # (cm,cm)
 SCREENDIST = 61.0 # cm
 PXPERCM = numpy.mean([DISPSIZE[0]/SCREENSIZE[0],DISPSIZE[1]/SCREENSIZE[1]]) # px/cm
 
-
 # # # # #
 # READ FILES
 
 # loop through all participants
-for ppname in PPS:
+for participant_id in PPS:
+    print("starting data analysis for participant '%s'" % (participant_id))
 
-	print("starting data analysis for participant '%s'" % (ppname))
+    # BEHAVIOUR
+    print("loading behavioural data")
 
-	# BEHAVIOUR
-	print("loading behavioural data")
+    participant = easy.get_participant(participant_id)
+    points = easy.get_points(participant['task1_start'], participant['task1_end'])
 
-	# path
-	fp = os.path.join(DATADIR, '%s.txt' % ppname)
+    # GAZE DATA
+    print("loading gaze data")
 
-	# open the file
-	fl = open(fp, 'r')
+    # NEW OUTPUT DIRECTORIES
+    # create a new output directory for the current participant
+    pplotdir = os.path.join(PLOTDIR, str(participant['name']))
 
-	# read the file content
-	data = fl.readlines()
+    # check if the directory already exists
+    if not os.path.isdir(pplotdir):
+        # create it if it doesn't yet exist
+        os.mkdir(pplotdir)
 
-	# close the file
-	fl.close()
+    # # # # #
+    # PLOTS
 
-	# process file contents
-	for i in range(len(data)):
-		data[i] = data[i].replace('\n','').replace('\r','').replace('"','').split(SEP)
+    print("plotting gaze data")
+    fixations = []
+    x_points = []
+    y_points = []
+    imagefile = os.path.join(IMGDIR, 'bg3.jpg')
+    raw_file = os.path.join(pplotdir, 'raw')
+    scatter_file = os.path.join(pplotdir, 'fixations')
+    scanpath_file = os.path.join(pplotdir, 'scanpath')
+    heatmap_file = os.path.join(pplotdir, 'heatmap')
 
-	# GAZE DATA
-	print("loading gaze data")
+    previous_point = []
+    saccades = []
 
-	# path
-	# fp = os.path.join(DATADIR, '%s.asc' % ppname)
+    for point in points:
+        x_points.append(point['x'])
+        y_points.append(point['y'])
+        fixations.append([time.time(), time.time(), 1, int(float(point['x'])), int(float(point['y']))])
 
-	# check if the path exist
-	# if not os.path.isfile(fp):
-	# 	# if not, check if the EDF exists
-	# 	edfp = os.path.join(DATADIR, '%s.edf' % ppname)
-	# 	if os.path.isfile(edfp):
-	# 		# if it does, convert if usinf edf2asc.exe
-	# 		os.system("edf2asc %s" % edfp)
-	# 		# load ASCII
-	# 		fp = os.path.join(DATADIR, '%s.asc' % ppname)
-	# 	# if it does not exist, raise an exception
-	# 	else:
-	# 		raise Exception("No eye data file (neither ASC, nor EDF) file found for participant '%s' (tried paths:\nASC: %s\nEDF: %s" % (ppname, fp, edfp))
+        if len(previous_point) != 0:
+            saccades.append([time.time(), time.time(), 1, int(float(previous_point['x'])), int(float(previous_point['y'])), int(float(point['x'])), int(float(point['y']))])
 
-	# read the file
-	# edfdata[trialnr]['time'] = list of timestamps in trialnr
-	# edfdata[trialnr]['size'] = list of pupil size samples in trialnr
-	# edfdata = read_edf(fp, EDFSTART, stop=EDFSTOP, missing=INVALCODE, debug=False)
+        previous_point = point
 
-	# NEW OUTPUT DIRECTORIES
-	# create a new output directory for the current participant
-	pplotdir = os.path.join(PLOTDIR, ppname)
-	# check if the directory already exists
-	if not os.path.isdir(pplotdir):
-		# create it if it doesn't yet exist
-		os.mkdir(pplotdir)
-
-
-	# # # # #
-	# PLOTS
-
-	print("plotting gaze data")
-
-        fixations = []
-        x_points = []
-        y_points = []
-        imagefile = os.path.join(IMGDIR, 'bg3.jpg')
-        raw_file = os.path.join(pplotdir, 'raw')
-        scatter_file = os.path.join(pplotdir, 'fixations')
-        scanpath_file = os.path.join(pplotdir, 'scanpath')
-        heatmap_file = os.path.join(pplotdir, 'heatmap')
-
-        previous_point = []
-        saccades = []
-
-        for point in data:
-            x_points.append(point[0])
-            y_points.append(point[1])
-            fixations.append([time.time(), time.time(), 1, int(point[0]), int(point[1])])
-
-            if len(previous_point) != 0:
-                saccades.append([time.time(), time.time(), 1, int(previous_point[0]), int(previous_point[1]), int(point[0]), int(point[1])])
-
-            previous_point = point
-
-        draw_raw(x_points, y_points, DISPSIZE, imagefile=imagefile, savefilename=raw_file)
-        draw_fixations(fixations, DISPSIZE, imagefile=imagefile, durationsize=True, durationcolour=False, alpha=0.5, savefilename=scatter_file)
-        draw_scanpath(fixations, saccades, DISPSIZE, imagefile=imagefile, alpha=0.5, savefilename=scanpath_file)
-        draw_heatmap(fixations, DISPSIZE, imagefile=imagefile, durationweight=True, alpha=0.5, savefilename=heatmap_file)
-
-	# loop through trials
-	# for trialnr in range(len(data)):
-	#
-	# 	# load image name, saccades, and fixations
-	# 	imgname = data[trialnr][header.index("image")]
-	# 	saccades = edfdata[trialnr]['events']['Esac'] # [starttime, endtime, duration, startx, starty, endx, endy]
-	# 	fixations = edfdata[trialnr]['events']['Efix'] # [starttime, endtime, duration, endx, endy]
-	#
-	# 	# paths
-	# 	imagefile = os.path.join(IMGDIR,imgname)
-	# 	rawplotfile = os.path.join(pplotdir, "raw_data_%s_%d" % (ppname,trialnr))
-	# 	scatterfile = os.path.join(pplotdir, "fixations_%s_%d" % (ppname,trialnr))
-	# 	scanpathfile =  os.path.join(pplotdir, "scanpath_%s_%d" % (ppname,trialnr))
-	# 	heatmapfile = os.path.join(pplotdir, "heatmap_%s_%d" % (ppname,trialnr))
-	#
-	# 	# raw data points
-	# 	draw_raw(edfdata[trialnr]['x'], edfdata[trialnr]['y'], DISPSIZE, imagefile=imagefile, savefilename=rawplotfile)
-        #
-	# 	# fixations
-	# 	draw_fixations(fixations, DISPSIZE, imagefile=imagefile, durationsize=True, durationcolour=False, alpha=0.5, savefilename=scatterfile)
-	#
-	# 	# scanpath
-	# 	draw_scanpath(fixations, saccades, DISPSIZE, imagefile=imagefile, alpha=0.5, savefilename=scanpathfile)
-        #
-	# 	# heatmap
-	# 	draw_heatmap(fixations, DISPSIZE, imagefile=imagefile, durationweight=True, alpha=0.5, savefilename=heatmapfile)
+    draw_raw(x_points, y_points, DISPSIZE, imagefile=imagefile, savefilename=raw_file)
+    draw_fixations(fixations, DISPSIZE, imagefile=imagefile, durationsize=True, durationcolour=False, alpha=0.5, savefilename=scatter_file)
+    draw_scanpath(fixations, saccades, DISPSIZE, imagefile=imagefile, alpha=0.5, savefilename=scanpath_file)
+    draw_heatmap(fixations, DISPSIZE, imagefile=imagefile, durationweight=True, alpha=0.5, savefilename=heatmap_file)
---
+++ examples/atoms_of_confusion/database/easy.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import psycopg2
+import psycopg2.extras
+
+def get_connection():
+    return psycopg2.connect("dbname=easy user=postgres password=postgres")
+
+def get_participant(participant_id):
+    """ query data from the vendors table """
+    conn = None
+    try:
+        conn = get_connection()
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute("SELECT id, name, email, flow, start_datetime, end_datetime, task1_start, task1_end, task2_start, task2_end FROM experiments_participant WHERE id = %s ORDER BY id desc" % participant_id)
+        row = cur.fetchone()
+
+        cur.close()
+
+        return row
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+def get_points(start_datetime, end_datetime):
+    """ query data from the vendors table """
+    conn = None
+    try:
+        conn = get_connection()
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute("SELECT * FROM experiments_point WHERE datetime between '%s' and '%s' ORDER BY id desc" % (start_datetime, end_datetime))
+        row = cur.fetchone()
+        rows = []
+
+        while row is not None:
+            print row
+            rows.append(row)
+            row = cur.fetchone()
+
+        cur.close()
+
+        return rows
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+

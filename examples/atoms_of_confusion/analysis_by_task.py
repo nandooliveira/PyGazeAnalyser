40b69e21bc88c607c1a8526f563fb6e2f7ffb11a
Fernando Oliveira <nandooliveira.al@gmail.com>
2019-05-17 18:13:50 -0300

    new analysis

--- examples/atoms_of_confusion/analysis_by_task.py
+++ examples/atoms_of_confusion/analysis_by_task.py
@@ -6,18 +6,24 @@ __author__ = "Fernando Oliveira"
 
 # native
 import os
-import sys
-
-# custom
-from pygazeanalyser.gazeplotter import draw_fixations, draw_heatmap, draw_scanpath, draw_raw
-
 # external
 import time
+from datetime import datetime
+
+# custom
+from pygazeanalyser.gazeplotter import draw_fixations, draw_heatmap, draw_raw
+
+# external
 import numpy
 
-from database import easy
 from database.tasks import Task
-from database.points import Point
+
+os.system('reset')
+
+
+def c_time():
+    return "\033[94m %s \033[0m" % datetime.now().ctime()
+
 
 # # # # #
 # CONSTANTS
@@ -27,44 +33,37 @@ from database.points import Point
 DIR = os.path.dirname(__file__)
 IMGDIR = os.path.join(DIR, 'imgs')
 PLOTDIR = os.path.join(DIR, 'plots')
-OUTPUTFILENAME = os.path.join(DIR, "output.txt")
 
 # check if the image directory exists
 if not os.path.isdir(IMGDIR):
     raise Exception("ERROR: no image directory found; path '%s' does not exist!" % IMGDIR)
 
-# check if output directorie exist; if not, create it
+# check if output directories exist; if not, create it
 if not os.path.isdir(PLOTDIR):
     os.mkdir(PLOTDIR)
 
 # EXPERIMENT SPECS
-DISPSIZE = (1920,1080) # (px,px)
+DISPSIZE = (1920, 1080)  # (px,px)
 SCREENSIZE = (39.9,29.9) # (cm,cm)
 SCREENDIST = 61.0 # cm
-PXPERCM = numpy.mean([DISPSIZE[0]/SCREENSIZE[0],DISPSIZE[1]/SCREENSIZE[1]]) # px/cm
+PXPERCM = numpy.mean([DISPSIZE[0]/SCREENSIZE[0], DISPSIZE[1]/SCREENSIZE[1]])  # px/cm
 
-# BACKGROUND IMAGE
-while True:
-    background_image = raw_input("\n\nPut the BG file at " + IMGDIR + " folder and type its name here (Make sure it is a jpg file): ") + '.png'
-    if os.path.exists(os.path.join(IMGDIR, background_image)):
-        print "Background image successfully found!..."
-        break
-    else:
-        print "\n\nI did not find the file at, " + str(os.path.join(IMGDIR, background_image))
-
-
-print 'get task and points..\n'
+print('%s: Get task and points' % c_time())
 
 # loop through all tasks
 for task in Task.by_experiment(2):
-    print("starting data analysis for task '%s'" % (task['description']))
+    print("%s: Starting data analysis for task '%s'" % (c_time(), task['description'],))
+
+    print("%s: Loading bg image" % c_time())
+    background_image = task['description'] + '.jpg'
+    if os.path.exists(os.path.join(IMGDIR, 'newimages', background_image)):
+        print("%s: Background image successfully found!" % c_time())
+    else:
+        raise ValueError("Could not find bg image at %s" % str(os.path.join(IMGDIR, background_image)))
 
     # LOAD POINTS
-    print("loading points from database...")
+    print("%s: Loading points from database" % c_time())
     points = Task.all_points(task['id'])
-
-    # GAZE DATA
-    print("loading gaze data")
 
     # NEW OUTPUT DIRECTORIES
     # create a new output directory for the current participant
@@ -78,11 +77,11 @@ for task in Task.by_experiment(2):
     # # # # #
     # PLOTS
 
-    print("plotting gaze data")
+    print("%s: Plotting gaze data" % c_time())
     fixations = []
     x_points = []
     y_points = []
-    imagefile = os.path.join(IMGDIR, background_image)
+    imagefile = os.path.join(IMGDIR, 'newimages', background_image)
     raw_file = os.path.join(pplotdir, 'raw')
     scatter_file = os.path.join(pplotdir, 'fixations')
     scanpath_file = os.path.join(pplotdir, 'scanpath')
@@ -92,23 +91,28 @@ for task in Task.by_experiment(2):
     saccades = []
 
     for point in points:
-        x = float(point['x'].replace(',', '.'))
-        y = float(point['y'].replace(',', '.'))
-        print "%f %f" % (x, y,)
+        x = int(float(point['x'].replace(',', '.')))
+        y = int(float(point['y'].replace(',', '.')))
 
         x_points.append(x)
         y_points.append(y)
 
         fixations.append([time.time(), time.time(), 1, x, y])
 
-        if len(previous_point) != 0:
-            previous_x = float(previous_point['x'].replace(',', '.'))
-            previous_y = float(previous_point['y'].replace(',', '.'))
-            saccades.append([time.time(), time.time(), 1, previous_x, previous_y, x, y])
+        # if len(previous_point) != 0:
+        #     previous_x = int(float(previous_point['x'].replace(',', '.')))
+        #     previous_y = int(float(previous_point['y'].replace(',', '.')))
+        #     saccades.append([time.time(), time.time(), 1, previous_x, previous_y, x, y])
 
         previous_point = point
 
+    print("%s: Generating Heat Map" % c_time())
     draw_heatmap(fixations, DISPSIZE, imagefile=imagefile, durationweight=True, alpha=0.5, savefilename=heatmap_file)
+
+    print("%s: Generating Raw Chart" % c_time())
     draw_raw(x_points, y_points, DISPSIZE, imagefile=imagefile, savefilename=raw_file)
-    draw_fixations(fixations, DISPSIZE, imagefile=imagefile, durationsize=True, durationcolour=False, alpha=0.5, savefilename=scatter_file)
-    draw_scanpath(fixations, saccades, DISPSIZE, imagefile=imagefile, alpha=0.5, savefilename=scanpath_file)
+
+    print("%s: Generating Fixations Map" % c_time())
+    draw_fixations(fixations, DISPSIZE, imagefile=imagefile, durationsize=True, durationcolour=False, alpha=0.5,
+                   savefilename=scatter_file)
+    # draw_scanpath(fixations, saccades, DISPSIZE, imagefile=imagefile, alpha=0.5, savefilename=scanpath_file)
